9c7b9af034c27a1183bdb37ba1ff606746636755
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-12-05 16:12:11 -0500

    Imported orm to create functions which will interact with the database

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+// Imported the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+const burgers = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgers_Controller.js).
+module.exports = burgers;
