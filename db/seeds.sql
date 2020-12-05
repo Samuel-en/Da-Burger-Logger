bee13228ed1614c1e225e70c7b8d95d23e7b12ea
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-12-05 00:27:52 -0500

    seed test  values added

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Big Mac");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Lover", true);
+INSERT INTO burgers (burger_name) VALUES ("Bacon Lover");
